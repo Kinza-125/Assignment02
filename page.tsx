0e56ebc1b01c453129ecdead02660f70faa96393
Kinza-125 <ka8553200@gmail.com>
2024-10-15 00:38:36 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Hero from "./Hero/Hero"
+import Header from "./components/Header/Header"
+import Footer from "./Footer/Footer"
+
+const Page = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+      
+      
+    </div>
+  )
+}
+
+export default Page
