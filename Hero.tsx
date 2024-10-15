b81a6b36fdf7578d432e0133d49d1369bc163e71
Kinza-125 <ka8553200@gmail.com>
2024-10-15 00:52:47 -0700

    Add files via upload

---
+++ Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='section'>
+        <h1>Welcome to my website</h1>
+        <p>This is my First website</p>
+
+    </section>
+  )
+}
+
+export default Hero
