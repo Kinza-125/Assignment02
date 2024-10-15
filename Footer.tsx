6260921f11690ec1f33455e6c987bcd5f5cb171a
Kinza-125 <ka8553200@gmail.com>
2024-10-15 00:42:48 -0700

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <h1>
+            &copy;This is Footer components
+        </h1>
+    </footer>
+  )
+}
+export default Footer
